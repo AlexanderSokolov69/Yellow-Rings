56e4289fb80336e7936354da15d4629a1ec3c05b
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2021-11-19 17:27:49 +0300

    Multicolors rings

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import sys
-from random import randint
+from random import randint, choice
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5 import uic
 
+from Ui import Ui_Form
 
-class Window(QWidget):
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super(Window, self).__init__()
-        uic.loadUi('Ui.ui', self)
+        self.initUi()
+        
+    def initUi(self):
+        self.setupUi(self)
         self.go_draw = False
+        self.colors = ['yellow', 'green', 'red', 'blue', 'brown', 'white']
         self.pushButton.clicked.connect(self.make)
         self.w = self.size().width() // 2
         self.h = self.size().height() // 2
@@ -23,7 +28,7 @@ class Window(QWidget):
         if self.go_draw:
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QColor('yellow'))
+            qp.setPen(QColor(choice(self.colors)))
             d = randint(10, min([self.h, self.w]))
             qp.drawEllipse(self.w - d, self.h - d, d * 2, d * 2)
             qp.end()
